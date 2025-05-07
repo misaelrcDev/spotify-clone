1def6f3d88d0823098986b233a0e88a0bd4eae16
Misael Cabral <misaelrc.dev@gmail.com>
2025-05-07 20:24:59 -0300

    first commit

---
+++ src/App2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+// const App2 = () => {
+//     return <h1>Olá, Mundo!</h1>;
+//   };
